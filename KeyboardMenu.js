6fd83ae015d6d98d6466b62b7625acc6f4d48c52
dim <dimx88@zoho.com>
2023-01-13 13:42:34 +0200

    keyboard controll added to menu navigation

--- KeyboardMenu.js
+++ KeyboardMenu.js
@@ -11,7 +11,7 @@ class KeyboardMenu {
         this.element.innerHTML = this.options.map((option, index) => {
             const disabledAttr = option.disabled ? 'disabled' : '';
             const autoFocusAttr = index === 0 ? 'autofocus' : '';
-            
+
             return (`
             <div class="option">
                 <button ${disabledAttr} ${autoFocusAttr} data-button="${index}" data-description="${option.description}">
@@ -50,9 +50,35 @@ class KeyboardMenu {
 
     }
 
+    end() {
+        // Remove menu & description elements
+        this.element.remove();
+        this.descriptionElement.remove();
+
+        // Clean up bindings
+        this.up.unbind();
+        this.down.unbind();
+    }
+
     init(container) {
         this.createElement();
         container.appendChild(this.descriptionElement);
         container.appendChild(this.element);
+
+        this.up = new KeyPressListener('ArrowUp', () => {
+            const current = Number(this.prevFocus.getAttribute('data-button'));
+            const prevButton = Array.from(this.element.querySelectorAll('button[data-button]')).reverse().find(el => {
+                return el.dataset.button < current && !el.disabled;
+            });
+            prevButton?.focus();
+        });
+
+        this.down = new KeyPressListener('ArrowDown', () => {
+            const current = Number(this.prevFocus.getAttribute('data-button'));
+            const nextButton = Array.from(this.element.querySelectorAll('button[data-button]')).find(el => {
+                return el.dataset.button > current && !el.disabled;
+            });
+            nextButton?.focus();
+        });
     }
 }
